b025ea446bf37ea0ba32fce5ef5c03b5328c8ea0
Andy Crowe <crowemi@hotmail.com>
2024-03-03 21:07:00 -0800

    updated aws ses

--- aws/client.go
+++ aws/client.go
@@ -3,18 +3,17 @@ package aws
 import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/ses"
 )
 
+// Client is the base AWS client containing configuration
 type Client struct {
 	Session *session.Session
 }
 
 func (client *Client) CreateSession(config *aws.Config) {
-	client.Session, _ = session.NewSession(config)
-}
-
-func (client *Client) GetSES(session session.Session) *ses.SES {
-	// if session passed in, use session else use AwsClient.Session
-	return ses.New(client.Session)
+	var err error
+	client.Session, err = session.NewSession(config)
+	if err != nil {
+		// handle session error, panic?
+	}
 }
